0015b6016786ef19a1bc8b25d08c9988d24f5235
Gustavo RM <gustavo.rm.isc@gmail.com>
2023-02-02 00:41:44 -0600

    Serve a ticket and show it in the screen

--- public/js/window.js
+++ public/js/window.js
@@ -1,3 +1,65 @@
+//HTML References
+const lblWindow = document.querySelector('h1');
+const btnServeTicket = document.querySelector('button');
+const lblTicket = document.querySelector('small');
+const divAlert = document.querySelector('.alert');
 
+//Getting the params from the url
+const searchParams = new URLSearchParams( window.location.search );
 
-console.log('Escritorio HTML');
+//If the params does not have the word window then redirect to the index.html
+if( !searchParams.has( 'window' ) ){
+    window.location = 'index.html';
+    throw new Error('The window is required');
+}
+
+//Get the word from the params
+const currentWindow = searchParams.get( 'window' );
+lblWindow.innerText = currentWindow;
+
+divAlert.style.display = 'none';
+
+//Client socket
+//comes from the library of the html
+const socket = io();
+
+//Listeners to events or changes
+//This listener helps to know when is connected to the server
+socket.on( 'connect', () => {
+    btnServeTicket.disabled = false;
+});
+
+//This listener helps to know when is disconnected to the server
+socket.on( 'disconnect', () => {
+    btnServeTicket.disabled = true;
+});
+
+//Getting the last ticket sent by the server
+socket.on( 'last-ticket', ( lastTicket ) => {
+    // lblNewTicket.innerText = `Ticket ${lastTicket}`;
+});
+
+//Adding an event listener to the button
+btnServeTicket.addEventListener( 'click', () => {
+
+    //Here we ask the server to send the next ticket (in the payload) to attend in the current window
+    socket.emit( 'serve-ticket', { currentWindow }, ( { ok, ticket} ) => {
+        
+        //In the callback we receive the ticket and the ok
+        //If ok is false it means that there is no more tickets
+        if( !ok ){
+            lblTicket.innerText = 'No one';
+            return divAlert.style.display = '';
+        }
+
+        //If there is more tickets we show them in the label
+        lblTicket.innerText = `Ticket ${ticket.number}`;
+
+    });
+    
+    //Getting the reference of the ticket sent by the server
+    // socket.emit( 'next-ticket', null, ( ticket ) => {
+    //     lblNewTicket.innerText = ticket;
+    // });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -22,5 +22,38 @@ export const socketController = ( socket ) => {
         //TODO: Notify there is a pending ticket to assign
 
     });
+
+    /**
+     * Serve the next ticket
+     * Receives the current window sent by the client
+     */
+    socket.on( 'serve-ticket', ( { currentWindow }, callback ) => {
+        
+        //If there is no window in the payload we return an error
+        if( !currentWindow ){
+            return callback({
+                ok: false,
+                msg: 'The window is requred'
+            });
+        }
+        
+        //Calling the method to serve the next ticket
+        const ticket = ticketControl.serveTicket( currentWindow );
+
+        //If there is no more tickets to serve then we return an error
+        if( !ticket ){
+            callback({
+                ok: false,
+                msg: 'There is no pending tickets'
+            });
+        }else{
+            //If there is pending tickets then we return the current ticket that is being served
+            callback({
+                ok: true,
+                ticket
+            });
+        }
+
+    });
     
 }
