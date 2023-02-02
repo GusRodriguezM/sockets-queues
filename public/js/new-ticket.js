f3e7ae35b190061b8761266aa93c847fcc092c7c
Gustavo RM <gustavo.rm.isc@gmail.com>
2023-02-01 23:37:46 -0600

    Showing the next ticket in the page

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,34 @@
+//HTML References
+const lblNewTicket = document.querySelector('#lblNewTicket');
+const btnCreate = document.querySelector('button');
 
 
-console.log('Nuevo Ticket HTML');
+//Client socket
+//comes from the library of the html
+const socket = io();
+
+//Listeners to events or changes
+//This listener helps to know when is connected to the server
+socket.on( 'connect', () => {
+    btnCreate.disabled = false;
+});
+
+//This listener helps to know when is disconnected to the server
+socket.on( 'disconnect', () => {
+    btnCreate.disabled = true;
+});
+
+//Getting the last ticket sent by the server
+socket.on( 'last-ticket', ( lastTicket ) => {
+    lblNewTicket.innerText = `Ticket ${lastTicket}`;
+});
+
+//Adding an event listener to the button
+btnCreate.addEventListener( 'click', () => {
+    
+    //Getting the reference of the ticket sent by the server
+    socket.emit( 'next-ticket', null, ( ticket ) => {
+        lblNewTicket.innerText = ticket;
+    });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,27 +1,25 @@
 import TicketControl from "../models/ticket-control.js";
 
+//Instance of the class TicketControl
 const ticketControl = new TicketControl();
 
+//Controller of the socket
 export const socketController = ( socket ) => {
 
     socket.on( 'disconnect', () => {
         console.log('Client disconnected...', socket.id);
     });
 
-    /**
-     * This listens when a client emits a message and the callback is the things we want to do when the client emits the message
-     * The first arg of the callback is the payload
-     * Th second arg is a callback where we can send data to the client
-     */
-    socket.on( 'send-message', ( payload, callback ) => {
+    //Sending the last ticket to the client
+    socket.emit( 'last-ticket', ticketControl.lastTicket );
 
-        // console.log('Message from the "client" received: ', payload);
+    //Getting the next ticket and sending through the callback to the client
+    socket.on( 'next-ticket', ( payload, callback ) => {
         
-        const id = '123ABC';
-        callback( id );
-        
-        //Sending a message to all the connected clients
-        socket.broadcast.emit( 'send-message', payload );
+        const nextTicket = ticketControl.nextTicket();
+        callback( nextTicket );
+
+        //TODO: Notify there is a pending ticket to assign
 
     });
     
